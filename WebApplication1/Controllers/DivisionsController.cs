a3b47d31e3d56285286986d3067b7104a13d7984
Desktop <adk_bond@hotmail.com>
2020-05-27 19:18:21 +0700

    Edited

---
+++ WebApplication1/Controllers/DivisionsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DivisionsController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            TestContext context = new TestContext();
+            var divisions = context.Divisions.ToList();
+            
+
+            return Json(divisions);
+            
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Divisions divisions)
+        {
+            TestContext context = new TestContext();
+            context.Divisions.Add(divisions);
+            context.SaveChanges();
+
+            return Json(divisions);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public string  Index(int id = 0)
+        {
+            return "Hello World." + id ;
+        }
+
+        public string Hello(string name ="")
+        {
+            return "Hello "+name;
+        }
+        public IActionResult Person()
+        {
+            return View();
+        }
+        public IActionResult Page1()
+        {
+            return View();
+        }
+    }
+
+}
